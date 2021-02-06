00a447dcd2eb5d7e66a460047f0e5651e9fb935f
nyood <undertowna@gmail.com>
2021-02-06 17:48:00 +0500

    travis

--- travis/package.sh
+++ travis/package.sh
@@ -8,14 +8,21 @@ cd $SOURCES
 derictories=($(ls -d */))
 
 for dir in "${derictories[@]}"; do
-    module=$SOURCES'/'$module
+    echo "Dir: ${dir}"
+
     module_name=$(basename -- "$dir")
+    echo "Module name: ${module_name}"
+
+    module=$SOURCES'/'$module_name
+    echo "Module: ${module}"
 
     cd $module
+    echo $PWD
     
     tar -czvf ${module_name}-${BUILD_NUMBER}.tar.gz *
     cp ${module_name}-${BUILD_NUMBER}.tar.gz ../
 
     cd ../
+    echo $PWD
     rm -Rfv $dir
 done
